0e3276ccfa4fcc0c000f361562361d86ba069d4a
sirMelka <melkabotale1@gmail.com>
2022-10-30 23:46:28 +0100

    feature: adding vercel.json file for deployment

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-const express = require('express')
-const http = require('http')
-const cors = require('cors')
-const passport = require('passport')
-const users = require('./routes/users')
-const messages = require('./routes/messages')
-const {Server} = require('socket.io') 
-require('./middlewares/authentification')
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const passport = require("passport");
+const users = require("./routes/users");
+const messages = require("./routes/messages");
+const { Server } = require("socket.io");
+require("./middlewares/authentification");
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:"*",
-        methods:["GET","POST","PUT","DELETE"]
-    }
-}) 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 
-io.on('connection', (socket)=>{
+io.on("connection", (socket) => {
+  console.log("user " + socket.id + " connected");
 
-    console.log('user '+socket.id+' connected')
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
 
-    socket.on('disconnect', ()=>{
+  socket.on("sendmessage", (data) => {
+    console.log(data);
+    socket.broadcast.emit("newmessage", data);
+  });
+});
 
-        console.log('disconnected')
-    })
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
 
-    socket.on("sendmessage",(data)=>{
-        console.log(data)
-        socket.broadcast.emit('newmessage', data)
-    })
+const restrictor = passport.authenticate("jwt", { session: false });
 
+app.use("/users/", users);
+app.use("/api/messages/", restrictor, messages);
 
-})
-
-app.use(cors())
-app.use(express.json())
-app.use(passport.initialize())
-
-const restrictor = passport.authenticate('jwt',{session:false})
-
-app.use('/users/', users)
-app.use('/api/messages/',restrictor, messages)
-
-server.listen(3001, ()=>{
-    console.log('server running on port 3001');
-})
+server.listen(3001, () => {
+  console.log("server running on port 3001");
+});
